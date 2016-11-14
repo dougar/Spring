b964024e6358e09b451998a5c542a5d962d925af
Clayton Rodrigo <clayton-dias@bol.com.br>
2016-11-14 19:58:37 -0200

    Spring-DataJPA
    tutorial

---
+++ SpringDataJPA/src/main/java/hello/Application.java
@@ -0,0 +1,55 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return (args) -> {
+			// Salver um par de clientes
+			repository.save(new Customer("Jack", "Bauer"));
+			repository.save(new Customer("Chloe", "O'Brian"));
+			repository.save(new Customer("Kim", "Bauer"));
+			repository.save(new Customer("David", "Palmer"));
+			repository.save(new Customer("Michelle", "Dessler"));
+
+			// Buscar todos os clientes
+			log.info("Clientes encontrados com findAll():");
+			log.info("-------------------------------");
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+            log.info("");
+
+			// Buscar um cliente individual por ID
+			Customer customer = repository.findOne(1L);
+			log.info("Clientes encontrados com findOne(1L):");
+			log.info("--------------------------------");
+			log.info(customer.toString());
+            log.info("");
+
+			// Buscar clientes por último nome
+			log.info("Clientes encontrados com findByLastName('Bauer'):");
+			log.info("--------------------------------------------");
+			for (Customer bauer : repository.findByLastName("Bauer")) {
+				log.info(bauer.toString());
+			}
+            log.info("");
+		};
+	}
+
+}
